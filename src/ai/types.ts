169a1eba304a0ae124b45b8ea416a35dcf921c5e
Sajan Rajbhandari <sazanrjb@gmail.com>
2025-06-16 23:59:08 +0545

    debug

--- src/ai/types.ts
+++ src/ai/types.ts
@@ -10,3 +10,13 @@ const User = {
   SYSTEM: Role.SYSTEM,
 }
 console.log({ User, Role })
+
+export enum ABC {
+  SYSTEM = 'system',
+  USER = 'user',
+  ASSISTANT = 'assistant',
+}
+
+const ABC = {}
+
+console.log(ABC)
